b91bb08b81e1df68e3f14ef9d34d62a79836cdbc
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-30 15:06:14 +0100

    :sparkles: Create validate Id middleware
    Use it to fetch single post

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -8,8 +8,8 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json({error: err, message: "Failed to hit server"}))
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id',validatePostId, (req, res) => {
+    res.status(201).json(req.posts)
 });
 
 router.delete('/:id', (req, res) => {
@@ -23,7 +23,16 @@ router.put('/:id', (req, res) => {
 // custom middleware
 
 function validatePostId(req, res, next) {
-
-};
+    const { id } = req.params;
+    postsDb.getById(id)
+    .then(posts => {
+        if(posts) {
+            req.posts = posts;
+            next();
+        }
+        else res.status(404).json({message: "Post with that ID exists not"})
+    })
+    .catch(err => res.status(500).json({error: err, message: "Failed to hit server"}))
+}
 
 module.exports = router;
