c0726c48d782eb939f532e7406ade18b4a2ac4af
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-30 16:36:38 +0100

    :sparkles: More MOre MORE!!!! endpoints

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,68 +1,75 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const userDb = require('./userDb')
+const userDb = require("./userDb");
 
-router.post('/', (req, res) => {
-    
+router.post("/", validateUser, (req, res) => {
+  userDb
+    .insert(req.user)
+    .then(data => res.status(201).json(data))
+    .catch(err => res.status(500).json({ error: err.message }));
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
-    
-});
+router.post("/:id/posts", validateUserId, (req, res) => {});
 
-router.get('/', (req, res) => {
-    userDb.get()
+router.get("/", (req, res) => {
+  userDb
+    .get()
     .then(users => {
-        res.status(201).json(users);
+      res.status(201).json(users);
     })
-    .catch(err => res.status(500).json({error: err.message}))
+    .catch(err => res.status(500).json({ error: err.message }));
 });
 
-router.get('/:id', validateUserId, (req, res) => {
-    res.status(201).json({message: "Success", data: req.user})
+router.get("/:id", validateUserId, (req, res) => {
+  res.status(201).json({ message: "Success", data: req.user });
 });
 
-router.get('/:id/posts', validateUserId, (req, res) => {
-    userDb.getUserPosts(req.user.id)
+router.get("/:id/posts", validateUserId, (req, res) => {
+  userDb
+    .getUserPosts(req.user.id)
     .then(posts => {
-        res.status(201).json(posts)
+      res.status(201).json(posts);
     })
-    .catch(err => res.status(500).json({error: err.message}))
+    .catch(err => res.status(500).json({ error: err.message }));
 });
 
-router.delete('/:id', validateUserId, (req, res) => {
-    userDb.remove(req.user.id)
-    .then(flag => {
-        if (flag) {
-            res.status(201).json({message: "deleted", data: req.user})
-        }
-    })
+router.delete("/:id", validateUserId, (req, res) => {
+  userDb.remove(req.user.id).then(flag => {
+    if (flag) {
+      res.status(201).json({ message: "deleted", data: req.user });
+    }
+  });
 });
 
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-    userDb.getById(req.params.id)
-    .then(user => {
-        if (user) {
-            req.user = user;
-            next()
-        } else res.status(404).json({message: "No user by that ID exists"})
-    })
+  userDb.getById(req.params.id).then(user => {
+    if (user) {
+      req.user = user;
+      next();
+    } else res.status(404).json({ message: "No user by that ID exists" });
+  });
 }
 
 function validateUser(req, res, next) {
+  if (!Object.keys(req.body).length) {
+    res.status(400).json({ message: "missing user data!" });
+  } else if (!req.body.name) {
+    res.status(400).json({ message: 'missing required "name" field!' });
+  } else {
+    const user = {
+      name: req.body.name
+    };
+    req.user = user;
+    next();
+  }
+}
 
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
